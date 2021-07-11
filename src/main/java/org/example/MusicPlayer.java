27a4e1be9dfaf52d6d46c3124dfaed4e195423f2
Артём Алилуев <tema_aliluev@outlook.com>
2021-07-11 20:16:12 +0300

    Spring

---
+++ src/main/java/org/example/MusicPlayer.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class MusicPlayer {
+    public Music music;
+
+    public MusicPlayer(Music music){
+        this.music=music;
+    }
+}
---
+++ src/main/java/org/example/TestSpring.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+       Music music=context.getBean("MusicBean",Music.class);
+       Music music1=context.getBean("MusicBean2",Music.class);
+    MusicPlayer musicPlayer=new MusicPlayer(music);
+    MusicPlayer musicPlayer1=new MusicPlayer(music1);
+       System.out.println(musicPlayer.music.getSong());
+       System.out.println(musicPlayer1.music.getSong());
+    context.close();
+    }
+}
